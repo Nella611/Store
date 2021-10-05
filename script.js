9f2eace16f987114f3e757df3c3ab6b0cda61b63
Nella <nella611@yandex.ru>
2021-10-05 14:56:25 +0200

    lesson2

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-onload = function () {
-  const goods = [
+class GoodsItem {
+
+  constructor(title='Товар', price=0) {
+    this.title = title;
+    this.price = price;
+  }
+
+  render() {
+    return `
+      <div class="goods-item">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+      </div>
+      `;
+  }
+
+}
+
+class GoodsList {
+
+  constructor() {
+    this.goods = [];
+    this.fetchGoods();
+    this.render();
+  }
+
+  fetchGoods() {
+    this.goods = [
     { title: 'Socks', price: 50 },
     { title: 'Jackets', price: 320 },
     { title: 'Shoes', price: 500 },
@@ -9,16 +35,55 @@ onload = function () {
     { title: 'T-shirts', price: 350 },
     { title: 'Sweaters', price: 450 },
     { title: 'Jeans', price: 260 },
-  ];
-  
-  const renderGoodsItem = (title='Товар', price='500') => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    ];
   }
-  
-  renderGoodsList(goods);
+
+  sumPrices() {
+    this.prices = this.goods.map(good => good.price);
+    this.sumPrices = this.prices.reduce((partial_sum, a) => partial_sum + a,0);
+    return this.sumPrices;
+  }
+
+  render() {
+    const goodsItems = this.goods.map(({title, price}) => {
+      const goodsItem = new GoodsItem(title, price);
+      return goodsItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = goodsItems.join('');
+  }
+
+}
+
+class goodInBasket {
+//явно структура должна быть как и у товара, но не знаю, должна ли наследоваться от товара
+  constructor(title='Товар', price=0) {
+    this.title = title;
+    this.price = price;
+  }
+}
+
+class Basket {
+  constructor() {
+    this.basket = [];
+  }
+
+  addGood(good) {
+//добавление в корзину
+  }
+
+  delGood(good) {
+//удаление из корзины
+  }
+
+  calculateGoods(){
+//  возвращает количество товаров в корзине
+  }
+
+  sumPrices() {
+//  возвращает сумму всех товаров в корзине
+  }
+}
+
+onload = () => {
+  const goodsList = new GoodsList();
 }
